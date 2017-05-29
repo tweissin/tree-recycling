f245e691864acaed4eb68e1a1dd0bb53852a0829
Thomas Weissinger <tweissin@cisco.com>
2017-05-29 18:30:12 -0400

    delete files

--- tree-website/php/db-get-dates.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('db-utils.php');
-
-header("Content-Type: application/json");
-$data = get_rows("dates");
-print json_encode($data);
--- tree-website/php/db-get-pickups.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('db-utils.php');
-
-header("Content-Type: application/json");
-$data = get_rows("tom_tmp_pickup");
-print json_encode($data);
