30251e9c1ee6872a5f54bcebd892d729632cdf4f
Thomas Weissinger <tweissin@cisco.com>
2018-12-04 05:17:46 -0500

    change to redirect to new website

---
+++ tree-website/index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <meta http-equiv="refresh" content="2; url=https://www.troop4hopkinton.com/tree-collection"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container">
+    <h1>Troop 4 Hopkinton Tree Collection</h1>
+    <p>The Christmas tree collection webpage may now be found here. You will be redirected.</p>
+
+    <p>If you are not automatically redirected, please click here:</p>
+
+    <a href="https://www.troop4hopkinton.com/tree-collection">https://www.troop4hopkinton.com/tree-collection</a><p>
+
+    <p>Update your bookmarks!</p>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+</html>
